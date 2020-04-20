472eb6e9955dee6255a5f8b6f29d0b77139a5dc2
Anony Kagamine <16967744+AnonyKagamine@users.noreply.github.com>
2020-04-20 05:51:26 +0000

    Support selecting version of data.json

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@
 // will be array
 fortunes = null;
 bosh = null;
+const default_title = "The Positive Messenger in UTF-8";
 
 function initDataByJsonText(text) {
     var jsonobj = JSON.parse(text);
@@ -37,17 +38,19 @@ function initDataByJsonText(text) {
     }
 }
 
-function loadData(json_url="./data.json")
+function loadData(json_url="./data.json", sync=false)
 {
     var req = new XMLHttpRequest();
-    req.open('GET', json_url);
+    req.open('GET', json_url, sync);
 
     req.onload = function() {
         if (req.status == 200) {
             //console.log(req.responseText);
             initDataByJsonText(req.responseText);
+            changeHeaderTitle("Complete!");
+            setTimeout(function(){changeHeaderTitle(default_title)}, 3000);
         } else {
-            console.log("Failed");
+            console.log("Failed:", req.status);
         }
     };
 
@@ -55,12 +58,58 @@ function loadData(json_url="./data.json")
     req.onerror = function() {
         console.log("Network Error");
     };
-
+    
+    // Notify user that we're loading
+    changeHeaderTitle("Loading bosh...");
     // Make the request
     req.send();
 }
 
+function changeHeaderTitle(text) {
+    document.getElementById("header_title").innerHTML = text;
+}
+
+function appendBoshDataAlternative(msgdataobj) {
+    var tr_entry = document.createElement("tr")
+    
+    var radio_element = document.createElement("input")
+    radio_element.type = "radio"
+    radio_element.name = "bosh_version"
+    radio_element.value = msgdataobj.url
+    radio_element.innerText = msgdataobj.title
+    radio_element.addEventListener("change", function(){loadData(this.value, true)})
+    
+    var title_td = document.createElement("td")
+    title_td.appendChild(radio_element);
+    tr_entry.appendChild(title_td)
+    
+    var provider_td = docuemnt.createElement("td")
+    provider_td.innerText = msgdataobj.provider
+    tr_entry.appendChild(provider_td)
+    
+    var desc_td = docuemnt.createElement("td")
+    desc_td.innerText = msgdataobj.description;
+    tr_entry.appendChild(desc_td);
+    
+    document.getElementById("boshdata").appendChild(tr_entry)
+}
+function loadMDI(mdi_url="../MessengerDataIndex/index.json") {
+    var req = new XMLHttpRequest();
+    req.open('GET', mdi_url);
+    req.onload = function() {
+        if (req.status == 200) {
+            var md_list = JSON.parse(req.responseText);
+            for (msgdataobj in md_list) {
+                appendBoshDataAlternative(msgdataobj);
+            }
+        } else {
+            console.log("Load MDI Failed:", req.status);
+        }
+    };
+}
+
 loadData();
+loadMDI();
 
 const decodedArea = document.getElementById('decoded-area');
 const encodedArea = document.getElementById('encoded-area');
